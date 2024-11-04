71f7a23572493ba79db9a8c2b85c34bee01323ef
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-11-04 14:31:40 +0530

    commit changes in cardsDto validation

--- src/main/java/com/tomcat/Cards/dto/CardsDto.java
+++ src/main/java/com/tomcat/Cards/dto/CardsDto.java
@@ -2,6 +2,10 @@ package com.tomcat.Cards.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,26 +21,35 @@ public class CardsDto {
     @Schema(
             description = "mobile number of the customer",example = "9123456789"
     )
+    @NotEmpty(message = "mobile number must not be null or empty")
+    @Pattern(regexp = "$![0-9]{10}",message = "mobile number must be of 10 digit only")
     private String mobileNumber;
 
     @Schema(
             description = "customers bank card number",example = "1122334455"
     )
+    @NotEmpty(message = "card number must not be null or empty")
+    @Pattern(regexp = "$![0-9]{10}",message = "card number must be of 10 digit only")
+    @Positive(message = "card number should be positive sequence of numbers")
     private String cardNumber;
 
     @Schema(
             description = "card type of the customer",example = "credit_card"
     )
+    @NotEmpty(message = "card type must not be null or empty")
+    @Size(min = 4, max = 16,message = "card type should be minimum of 4 character")
     private String cardType;
 
     @Schema(
             description = "2024-11-04T14:15:19.405+05:30",example = "9123456789"
     )
+    @NotEmpty(message = "card expiry date must not be null or empty")
     private LocalDateTime cardExpiryDate;
 
     @Schema(
             description = "card 3 digit cvv number",example = "123"
     )
+    @NotEmpty(message = "card cvv number must not be null or empty")
     private long cardCVV;
 
     @Schema(
