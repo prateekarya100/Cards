85b7e7b6bfa79fdf3b782c1b34c6a864a30b72d3
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-11-04 14:07:11 +0530

    code commit for cards repository changes

--- src/main/java/com/tomcat/Cards/repository/CardsRepository.java
+++ src/main/java/com/tomcat/Cards/repository/CardsRepository.java
@@ -1,7 +1,9 @@
 package com.tomcat.Cards.repository;
 
 import com.tomcat.Cards.model.Cards;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +14,7 @@ public interface CardsRepository extends JpaRepository<Cards, Long> {
 
     Optional<Cards> findByCardNumber(String cardNumber);
 
+    @Transactional
+    @Modifying
     Optional<Cards> deleteByMobileNumber(String mobileNumber);
 }
